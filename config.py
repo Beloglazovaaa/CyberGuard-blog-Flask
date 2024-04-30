97f56f6cb360e672b787188770409e06e643af22
Daniil <4i.danila@gmail.com>
2024-04-30 18:06:10 +0300

    Flask

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/test'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
