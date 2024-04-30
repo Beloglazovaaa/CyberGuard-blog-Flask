e27af6cd7cc6038844c687427157773b397cb5b5
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-30 15:46:26 +0300

    fixes

--- app/models.py
+++ app/models.py
@@ -15,7 +15,9 @@ class User(db.Model, UserMixin):
         return f'User: <{self.username}>'
 
 class Article(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    text = db.Column(db.Text(), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)  # Adding a title column
+    text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
