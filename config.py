52bab7f9dd3955ad6afd976b061f407d623c0cbe
Daniil <4i.danila@gmail.com>
2024-05-09 01:29:28 +0300

    Header fixed

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@db:3306/DB'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3307/DB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
